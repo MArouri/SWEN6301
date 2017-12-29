c8d5ae4ca979f70457c1ffad937897218062381b
mohammedabuaisha <1175033@student.birzeit.edu>
2017-12-29 11:11:20 +0200

    Prepare travis config and update project structure

---
+++ multipart-downloader/test/src/TestCase.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestCase {
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(true);
+        System.out.println("Test Done");
+
+    }
+}
